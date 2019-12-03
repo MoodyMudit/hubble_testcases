7ce10401c86601f18f6809829a11410b8530a00a
Mudit Agarwal <muditagr90@gmail.com>
2019-12-03 17:14:45 +0530

    Addint test cases

--- test_hubble.py
+++ test_hubble.py
@@ -1,5 +1,17 @@
 import helper
+import json
 
 def test_hubble():
   output = helper.execute_cmd(["/usr/bin/hubble","--version"])
   assert '3.0.7' in output
+
+def test_hubble_compliance():
+  output = helper.execute_cmd(["/usr/bin/hubble","hubble.audit","--json"])
+  object = json.loads(output)
+  compliance = object["Compliance"]
+  assert compliance != ''
+
+def test_hubble_grains_get():
+  output = helper.execute_cmd(["/usr/bin/hubble","grains.get", "host", "--json"])
+  object = json.loads(output)
+  assert object != ''
